e9ca5e94b78314a0cca1da1ad9d671725193a36f
cfeld7 <cbfeld99@gmail.com>
2021-05-04 16:41:50 -0400

    nothing

--- public/scripts.js
+++ public/scripts.js
@@ -5,3 +5,11 @@ async function windowActions() {
 }
 window.onload = windowActions;
 console.log('window loaded');
+
+async function windowActions() {
+  const request = await fetch('/api/stats');
+  stats= await request.json();
+  console.log(stats)
+}
+window.onload = windowActions;
+console.log('window loaded');
